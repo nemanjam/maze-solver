7f0c64bf1db115d618a8d647455e337d7fe6b13b
nemanjam <nemanja.mitic.elfak@hotmail.com>
2024-10-08 19:42:02 +0200

    comment DFS

--- src/solvers/maze-solver-dfs.ts
+++ src/solvers/maze-solver-dfs.ts
@@ -4,6 +4,9 @@ import { directions } from '../utils/constants';
 import { MazeSolver } from './maze-solver';
 
 export class MazeSolverDFS extends MazeSolver {
+  /**
+   * Implements the Depth-First Search (DFS) algorithm to find a path from the start to the end of the maze.
+   */
   protected findPath(): Coordinate[] | null {
     const start = this.maze.getStart();
 
@@ -15,15 +18,33 @@ export class MazeSolverDFS extends MazeSolver {
     while (stack.length > 0) {
       this.incrementStep();
 
+      /**
+       *
+       * The most important line. The only difference from BFS.
+       * LIFO - Takes the newest element from the stack.
+       *
+       * It will try to keep the same direction with priority from the directions array.
+       * Without walls and bounds it would run in straight line.
+       *
+       * Example for top, right, down, left.
+       *  4
+       *  3
+       *  2
+       *  1
+       *
+       * Mutates the stack. coord is current element.
+       *
+       */
       const { coord, path } = stack.pop()!;
 
       if (this.maze.isEnd(coord)) {
         return path;
       }
 
-      // Print the current state of the maze
+      // Print the current state of the maze.
       this.printBoard(coord, visited, path);
 
+      // Completely same as BFS.
       for (const direction of directions) {
         const nextCoord: Coordinate = {
           x: coord.x + direction.x,
