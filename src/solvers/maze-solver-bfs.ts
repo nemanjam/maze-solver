6bd93bf1a98f3d226f092130d8a8330bd46eaa2b
nemanjam <nemanja.mitic.elfak@hotmail.com>
2024-10-08 18:15:02 +0200

    comment BFS solver

--- src/solvers/maze-solver-bfs.ts
+++ src/solvers/maze-solver-bfs.ts
@@ -21,6 +21,25 @@ export class MazeSolverBFS extends MazeSolver {
       // Count iterations.
       this.incrementStep();
 
+      /**
+       *
+       * The most important line. Determines how it will choose the next element for test.
+       * FIFO - Takes the oldest element in the queue.
+       *
+       * It will try to change direction in each iteration. Same direction has the lowest priority.
+       * Without walls and bounds it would run in circle, because the for loop bellow loops directions in circle.
+       *
+       * Example for top, right, down, left.
+       *   6
+       * 7 2 8
+       * 5 1 3 9
+       *   4
+       *
+       * Keeps the current path snapshot for each element in the path, takes up a lot of memory.
+       *
+       * Mutates the queue. coord is current element.
+       *
+       */
       const { coord, path } = queue.shift()!;
 
       // Check if end and exit.
@@ -31,13 +50,17 @@ export class MazeSolverBFS extends MazeSolver {
       // Print the current state of the maze.
       this.printBoard(coord, visited, path);
 
+      // Always loops 4 times.
       for (const direction of directions) {
+        // Calculate the next coordinate by applying the direction.
         const nextCoord: Coordinate = {
           x: coord.x + direction.x,
           y: coord.y + direction.y,
         };
-
+        // Create a key for nextCoord (to check for uniqueness in the visited set).
         const coordKey = `${nextCoord.x},${nextCoord.y}`;
+
+        // If nextCoord is not visited, is within bounds, and is walkable, add it to the potential path.
         if (
           !visited.has(coordKey) &&
           this.maze.isWithinBounds(nextCoord) &&
