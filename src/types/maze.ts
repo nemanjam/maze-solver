17d7ab276535f2e764bc6682526c90e5dfc38040
nemanjam <nemanja.mitic.elfak@hotmail.com>
2024-10-08 13:21:27 +0200

    add comments for types

--- src/types/maze.ts
+++ src/types/maze.ts
@@ -1,8 +1,14 @@
+/**
+ * Represents a coordinate with x and y values.
+ * @property {number} x - The x-coordinate (row) of the point in the maze.
+ * @property {number} y - The y-coordinate (column) of the point in the maze.
+ */
 export interface Coordinate {
   readonly x: number;
   readonly y: number;
 }
 
+/** Public interface for the Maze class. */
 export interface IMaze {
   isWithinBounds: (coord: Coordinate) => boolean;
   isWalkable: (coord: Coordinate) => boolean;
--- src/types/solver.ts
+++ src/types/solver.ts
@@ -1,9 +1,12 @@
 import { Coordinate } from './maze';
 
+/** Public interface for a maze solver. */
 export interface IMazeSolver {
   solve(): string;
   solveAndFormat(): FormattedResult;
 }
+
+/** Human-friendly formatted result of a maze solver with additional information. */
 export interface FormattedResult {
   path: string;
   name: string;
@@ -11,19 +14,29 @@ export interface FormattedResult {
   formattedResult: string;
 }
 
+/**
+ * Direction for the next step.
+ * @property {number} x - Movement in the x direction (row).
+ * @property {number} y - Movement in the y direction (column).
+ */
 export interface Direction {
   x: number;
   y: number;
 }
 
+/** Base queue. */
 export interface QueueElement {
   coord: Coordinate;
   path: Coordinate[];
 }
+
+/** BFS queue. FIFO. */
 export interface BFSQueueElement extends QueueElement {}
 
+/** DFS queue. LIFO. */
 export interface DFSStackElement extends QueueElement {}
 
+/** Dijkstra and A* priority Queue with cost. */
 export interface DijkstraQueueElement extends QueueElement {
   cost: number;
 }
